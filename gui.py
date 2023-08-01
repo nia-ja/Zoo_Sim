5c89613ef4ae97547d75d41c284b5b8ad9218a8a
Kasia <kasia.bondarava@gmail.com>
2023-07-31 21:37:00 -0400

    Redraw whole screen on drawObj

--- gui.py
+++ gui.py
@@ -1,21 +1,22 @@
+import time
+
 import pygame
 import sys
 
 block_size = 20
 
 def grid_gui(x, y):
-    global screen, clock, WINDOW_HEIGHT, WINDOW_WIDTH
+    global screen, clock, window_hight, window_width
     # pygame setup
     pygame.init()
-    WINDOW_HEIGHT = y*block_size
-    WINDOW_WIDTH = x*block_size
-    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    window_hight = y*block_size
+    window_width = x*block_size
+    screen = pygame.display.set_mode((window_width, window_hight))
     clock = pygame.time.Clock()
     running = True
-    screen.fill("purple")
 
     while running:
-        drawGrid()
+        # drawGrid()
 
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
@@ -24,19 +25,27 @@ def grid_gui(x, y):
                 pygame.quit()
                 sys.exit()
 
-        pygame.display.update()
-
         obj = [(0,0), (1,3), (4,2), (2, 5)]
 
         drawObj(obj)
 
+        obj2 = [(0,0), (2, 5)]
+
+        drawObj(obj2)
+
+
 def drawGrid():
-    for x in range(0, WINDOW_WIDTH, block_size):
-        for y in range(0, WINDOW_HEIGHT, block_size):
+    for x in range(0, window_width, block_size):
+        for y in range(0, window_hight, block_size):
             rect = pygame.Rect(x, y, block_size, block_size)
             pygame.draw.rect(screen, "white", rect, 1)
 
 def drawObj(obj):
+    screen.fill("purple")
+    drawGrid()
     for x, y in obj[0:]:
         rect = pygame.Rect(x*block_size, y*block_size, block_size, block_size)
         pygame.draw.rect(screen, "black", rect)
+    
+    pygame.display.update()
+    time.sleep(3)
